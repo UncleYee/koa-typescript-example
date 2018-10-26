567ec2ccda365e9a9d668bff477b5ff319f5d34e
UncleYee <uncleye159@gmail.com>
2018-10-26 10:19:42 +0800

    add config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,41 +6,41 @@ const logger = getLogger('config')
 
 const config = convict({
   env: {
-    doc: "The application environment.",
-    format: ["production", "development", "test"],
-    default: "development",
-    env: "NODE_ENV"
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV'
   },
   ip: {
-    doc: "The IP address to bind.",
-    format: "ipaddress",
-    default: "127.0.0.1",
-    env: "IP_ADDRESS",
+    doc: 'The IP address to bind.',
+    format: 'ipaddress',
+    default: '127.0.0.1',
+    env: 'IP_ADDRESS',
   },
   port: {
-    doc: "The port to bind.",
-    format: "port",
+    doc: 'The port to bind.',
+    format: 'port',
     default: 8080,
-    env: "PORT",
-    arg: "port"
+    env: 'PORT',
+    arg: 'port'
   },
   db: {
     host: {
-      doc: "Database host name/IP",
+      doc: 'Database host name/IP',
       format: '*',
       default: '127.0.0.1'
     },
     name: {
-      doc: "Database name",
+      doc: 'Database name',
       format: String,
       default: 'users'
     }
   },
   system: {
     http_server_host: {
-      doc: "http server host",
+      doc: 'http server host',
       format: String,
-      default: '*' // HTTP服务器地址,请勿添加"http://" （即前端调用使用的服务器地址，如果是APP请设置为 * )
+      default: '*' // HTTP服务器地址,请勿添加'http://' （即前端调用使用的服务器地址，如果是APP请设置为 * )
     }
   }
 })
@@ -48,11 +48,10 @@ const config = convict({
 // 加载当前环境下的配置文件
 const env = config.get('env')
 try {
-  const path = `${__dirname}/${env}.json`
-
+  const path = `${__dirname}/env/${env}.json`
   logger.info(`trying to access ${path}`)
-
-  config.loadFile(path);
+  config.loadFile(path)
+  logger.info(`load ${env} config success`)
 } catch (error) {
   logger.warn(`${env} config file doesn\'t exist, loading defaults`)
 }
