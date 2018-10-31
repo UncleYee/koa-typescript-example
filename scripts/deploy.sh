ed064abb8cefe34ca17a1b00be5b8d9db178fb13
UncleYee <uncleye159@gmail.com>
2018-10-31 12:49:08 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,12 @@
-echo 1
+### server username
+$name = 'root' 
+### server host
+$host = '127.0.0.1'
+### 
+$path = '/path/to/production/folder'
+
+### transfer source code to server
+scp -r src $name@$host:$path
+
+### transfer package.json to server
+scp package.json $name@$host:$path
