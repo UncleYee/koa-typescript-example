7083f0e9c48b6b193036d0705f8cda8e9b95a0aa
UncleYee <uncleye159@gmail.com>
2018-11-06 14:16:25 +0800

    deploy bug fix & update tsconfig typeRoots

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,14 @@
 ### server username
-$name = 'root' 
+name = 'root' 
 ### server host
-$host = '127.0.0.1'
-### 
-$path = '/path/to/production/folder'
+host = '127.0.0.1'
+### target folder
+path = '/path/to/production/folder'
 
 ### transfer source code to server
 scp -r src $name@$host:$path
 
 ### transfer package.json to server
 scp package.json $name@$host:$path
+
+### transfer tsconfig.json to server
