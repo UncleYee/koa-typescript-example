1b6d8014ebadb23a6d5b544a522291f61ebd8986
UncleYee <uncleye159@gmail.com>
2019-01-29 11:25:35 +0800

    operationalias

--- src/service/sequelize.ts
+++ src/service/sequelize.ts
@@ -3,6 +3,44 @@ import { Sequelize } from 'sequelize-typescript'
 
 import config from '../config'
 
+const Op = Sequelize.Op
+const operatorsAliases = {
+  $eq: Op.eq,
+  $ne: Op.ne,
+  $gte: Op.gte,
+  $gt: Op.gt,
+  $lte: Op.lte,
+  $lt: Op.lt,
+  $not: Op.not,
+  $in: Op.in,
+  $notIn: Op.notIn,
+  $is: Op.is,
+  $like: Op.like,
+  $notLike: Op.notLike,
+  $iLike: Op.iLike,
+  $notILike: Op.notILike,
+  $regexp: Op.regexp,
+  $notRegexp: Op.notRegexp,
+  $iRegexp: Op.iRegexp,
+  $notIRegexp: Op.notIRegexp,
+  $between: Op.between,
+  $notBetween: Op.notBetween,
+  $overlap: Op.overlap,
+  $contains: Op.contains,
+  $contained: Op.contained,
+  $adjacent: Op.adjacent,
+  $strictLeft: Op.strictLeft,
+  $strictRight: Op.strictRight,
+  $noExtendRight: Op.noExtendRight,
+  $noExtendLeft: Op.noExtendLeft,
+  $and: Op.and,
+  $or: Op.or,
+  $any: Op.any,
+  $all: Op.all,
+  $values: Op.values,
+  $col: Op.col
+}
+
 export default () => {
   const { name, username, password, port, host, type } = config.get('db')
 
@@ -23,7 +61,7 @@ export default () => {
       min: 0
     },
     timezone: '+08:00', // 东八时区
-    operatorsAliases: false // 操作符别名
+    operatorsAliases // 操作符别名
   })
   // 加载所有模块
   sequelize.addModels([__dirname + '/**/*.model.ts'])
