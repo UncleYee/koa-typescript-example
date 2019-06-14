fa2ad0ecb32b153e8238fa55063bd0cbe95d441c
UncleYee <uncleye159@gmail.com>
2019-06-14 13:56:00 +0800

    add process exit wait

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import config from './config'
 
 async function run() {
   const app = require('./web/server').default
-  app.listen(config.get('port'))
+  const server = app.listen(config.get('port'))
   console.log(`app is listen at port ${config.get('port')}`)
+  ; ['SIGINT', 'SIGTERM'].forEach((sig: 'SIGINT' | 'SIGTERM') => process.on(sig, () => {
+    console.log(sig)
+    server.close(() => process.exit())
+  }))
 }
 
 run()
